e421aa39f88793ef7afd5874a9ddad4d74864761
VAV Laboratory <vavlaboratory.dev@gmail.com>
2026-09-19 13:32:33 -0300

    Use static Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: vercel(),
 
   redirects: {
     '/': '/Index'
